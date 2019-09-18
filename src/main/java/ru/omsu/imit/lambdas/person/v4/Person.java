b607766cb37d336ffd728967b53241a2e6ad12f7
Roman <romanbelinsky99@gmail.com>
2019-09-18 20:39:40 +0600

    Implement lambdas sortByNameLength and sortByNameCount in class v4.Person

--- src/main/java/ru/omsu/imit/lambdas/person/v4/Person.java
+++ src/main/java/ru/omsu/imit/lambdas/person/v4/Person.java
@@ -1,6 +1,10 @@
 package ru.omsu.imit.lambdas.person.v4;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Person {
     private String name;
@@ -26,6 +30,22 @@ public class Person {
     public int getAge() {
         return age;
     }
+
+    public static final Function<List<Person>, List<String>> sortByNameLength = l -> l.stream()
+            .filter(p -> p != null && p.age >= 30)
+            .map(p -> p.name)
+            .distinct()
+            .sorted(Comparator.comparing(String::length))
+            .collect(Collectors.toList());
+
+    public static final Function<List<Person>, List<String>> sortByNameCount = l -> l.stream()
+            .filter(p -> p != null && p.age >= 30)
+            .map(Person::getName)
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.toList()))
+            .values().stream()
+            .sorted(Comparator.comparing(List::size))
+            .map(strings -> strings.iterator().next())
+            .collect(Collectors.toList());
 
     @Override
     public boolean equals(Object o) {

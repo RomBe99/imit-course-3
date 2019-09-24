ffb5aeaf328604f2e517d23547b88571f6c20d9e
Roman <romanbelinsky99@gmail.com>
2019-09-24 21:38:53 +0600

    [+] method writeTraineeToFileWithByteBuffer in class TraineeProcessor [f] methods readTraineeFromTextFileWithByteBuffer and readTraineeFromTextFileWithMappedByteBuffer

--- src/main/java/ru/omsu/imit/nio/trainee/TraineeProcessor.java
+++ src/main/java/ru/omsu/imit/nio/trainee/TraineeProcessor.java
@@ -20,28 +20,53 @@ public class TraineeProcessor {
         }
     }
 
-    public static Trainee readTraineeFromTextFileWithByteBuffer(final File file) throws IOException, TraineeException {
-        try (FileChannel fc = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
-            if (fc.size() == 0) {
-                return null;
+    public static void writeTraineeToFileWithByteBuffer(final File file, final Trainee trainee) throws IOException {
+        final char[] FIRST_NAME_CHARS = trainee.getFirstName().toCharArray();
+        final char[] LAST_NAME_CHARS = trainee.getLastName().toCharArray();
+        final int BUFFER_SIZE = FIRST_NAME_CHARS.length * Character.SIZE / 8 + LAST_NAME_CHARS.length * Character.SIZE / 8 + Integer.SIZE / 8;
+
+        try (FileChannel fc = FileChannel.open(file.toPath(), StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE)) {
+            ByteBuffer bb = ByteBuffer.allocate(BUFFER_SIZE);
+
+            for (char c : FIRST_NAME_CHARS) {
+                bb.putChar(c);
             }
 
-            ByteBuffer bb = ByteBuffer.allocate((int) fc.size());
+            for (char c : LAST_NAME_CHARS) {
+                bb.putChar(c);
+            }
+
+            bb.putInt(trainee.getMark());
+            fc.write(bb.flip());
+        }
+    }
+
+    public static Trainee readTraineeFromTextFileWithByteBuffer(final File file) throws IOException, TraineeException {
+        if (file.length() == 0) {
+            return null;
+        }
+
+        try (FileChannel fc = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+            ByteBuffer bb = ByteBuffer.allocate((int) file.length());
 
             fc.read(bb);
 
-            String[] data = new String(StandardCharsets.UTF_8.decode(bb).array()).split(" ");
+            String[] data = new String(StandardCharsets.UTF_8.decode(bb.flip()).array()).split(" ");
 
             return new Trainee(data[0], data[1], Integer.parseInt(data[2]));
         }
     }
 
     public static Trainee readTraineeFromTextFileWithMappedByteBuffer(final File file) throws IOException, TraineeException {
+        if (file.length() == 0) {
+            return null;
+        }
+
         try (FileChannel fc = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
-            ByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            ByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
 
             if (mbb != null) {
-                String[] data = new String(StandardCharsets.UTF_8.decode(mbb).array()).split(" ");
+                String[] data = new String(StandardCharsets.UTF_8.decode(mbb).flip().array()).split(" ");
 
                 return new Trainee(data[0], data[1], Integer.parseInt(data[2]));
             }
